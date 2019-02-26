99daaedb6ed9a0cd490a7b7b5d263568976d5322
Oscar Munoz <omunozg@emory.edu>
2019-02-26 20:45:23 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -3,28 +3,33 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  var lyric = "maybe"
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
+
+  
 }
 
 function sayMyName() {
   // fix the code in here:
+  
   var name = "Cricky";
-
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
+  
+function sayMy() {
+    var name = "Kristin"
+    console.log(name)
   }
+  sayMy()
 }
+
+  /* function sayMy() {
+    console.log(name);
+    var name = "Kristin"; */
